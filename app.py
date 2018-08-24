f0833b26764869720a167360ca3e55d88ec38bd5
El Pirata Mike <mikefields81@gmail.com>
2018-08-23 20:35:16 -0500

    base app.py, jinja2 templates

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for, redirect
+#import flask server
+#render_template is template engine
+#url_for url generator and load static files
+#redirect
+
+app = Flask(__name__)
+#instantiate the server
+
+@app.route('/')
+def index():
+    fruits = ['Banana', 'Tomato', 'Kiwi', 'Mango']
+    return render_template('index.html', fruits=fruits)
+    # return url_for('index')
+
+@app.route('/about')
+def test():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4812)
+#debug mode is like nodemon
+#port5000 is the default
